31dd54964ff2a59d45c0d5ad05a28627c27e6522
DaQuan-Monte Quincy LeDarius III <65831888+kosmakyo@users.noreply.github.com>
2025-12-03 21:18:57 -0500

    added shield

--- Assets/_3DStealthGame/Scripts/PlayerMovement.cs
+++ Assets/_3DStealthGame/Scripts/PlayerMovement.cs
@@ -9,17 +9,23 @@ public class PlayerMovement : MonoBehaviour
 
     public float walkSpeed = 1.0f;
     public float turnSpeed = 20f;
+    public bool shield;
 
     Animator m_Animator;
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
+    
+    public GameObject shieldPrefab;
+    public GameObject ghostPrefab;
+
 
     void Start ()
     {
         m_Animator = GetComponent<Animator> ();
         m_Rigidbody = GetComponent<Rigidbody> ();
         MoveAction.Enable();
+        shield = false;
     }
 
     void FixedUpdate ()
@@ -41,5 +47,31 @@ public class PlayerMovement : MonoBehaviour
         
         m_Rigidbody.MoveRotation (m_Rotation);
         m_Rigidbody.MovePosition (m_Rigidbody.position + m_Movement * walkSpeed * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider whatDidIHit)
+    {
+        if(whatDidIHit.tag == "Powerup")
+        {
+            Destroy(whatDidIHit.gameObject);
+            if (shield == false)
+            {
+                shield = true;
+                shieldPrefab = Instantiate(shieldPrefab);
+                shieldPrefab.transform.parent = transform;
+                shieldPrefab.transform.position = new Vector3 (transform.position.x - 1.633f, transform.position.y - 1.248f, transform.position.z - 3.143f);
+            } else
+            {
+                
+            }
+        }
+        if(whatDidIHit.tag == "Enemy")
+        {
+            if (shield == true)
+            {
+                Destroy(whatDidIHit.gameObject);
+                shield = false;
+                Destroy(shieldPrefab);
+            }
+        }
     }
 }
